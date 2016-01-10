fcb41d76fbf73a9df9ae25b5a735bdb79f9b6028
Saheed Salam <saheed.salam@gmail.com>
2016-01-09 19:30:50 -0600

    Finish user sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  resources :users
   root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
